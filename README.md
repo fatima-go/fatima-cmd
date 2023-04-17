0312cf8de70efc2a534b194befcaec64f39a5c9d
dave <djin.chung@sk.com>
2023-04-17 17:43:58 +0900

    Project Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# fatima-cmd #
+
+provide useful cli commands in $FATIMA_HOME/bin<br>
+process with "ro" prefix means remote operating and "lc" means local.
--- juno/log.go
+++ juno/log.go
@@ -1,34 +1,31 @@
-//
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with p work for additional information
-// regarding copyright ownership.  The ASF licenses p file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use p file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-//
-// @project fatima-cmd
-// @author DeockJin Chung (jin.freestyle@gmail.com)
-// @date 2017. 10. 28. PM 2:52
-//
+/*
+ * Copyright 2023 github.com/fatima-go
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @project fatima-core
+ * @author jin
+ * @date 23. 4. 14. 오후 5:07
+ */
 
 package juno
 
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/fatima-go/fatima-cmd/share"
 	"sort"
 	"strings"
-	"throosea.com/fatima-cmd/share"
 )
 
 func PrintJunoLogLevels(flags share.FatimaCmdFlags) error {
